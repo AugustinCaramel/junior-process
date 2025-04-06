a0711063409cbcfafbf206842f9d4a9dcffc40fd
augustin_zahorka <augustin.zahorka@gmail.com>
2025-04-06 16:37:05 +0200

    Ajout des authentification jinnov

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { auth, provider } from "./firebase";
+import { signInWithPopup } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = useState<any>(null);
+  const [checkingAuth, setCheckingAuth] = useState(true);
+
+  const login = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = () => {
+    auth.signOut();
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      const email = user?.email ?? "";
+      const allowedDomain = "@jinnov-insa.fr";
+
+      if (user && email.endsWith(allowedDomain)) {
+        setUser(user);
+      } else {
+        if (user) {
+          alert("Accès réservé aux comptes @jinnov-insa.fr");
+        }
+        auth.signOut();
+      }
+
+      setCheckingAuth(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (checkingAuth) {
+    return <p className="p-6">Chargement...</p>;
+  }
+
+  if (!user) {
+    return (
+      <div className="p-6">
+        <button
+          onClick={login}
+          className="bg-blue-600 text-white px-4 py-2 rounded"
+        >
+          Connexion avec Google
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="p-6">
+      <div className="mb-4 flex items-center justify-between">
+        <div>Bonjour {user.displayName} ({user.email})</div>
+        <button onClick={logout} className="text-sm underline text-blue-600">
+          Déconnexion
+        </button>
+      </div>
+
       <nav className="mb-6 space-x-4">
         <Link to="/">Accueil</Link>
         <Link to="/cartographie">Cartographie</Link>
@@ -18,7 +81,6 @@ function App() {
   );
 }
 
-// Pages de test simples
 function Accueil() {
   return <h1 className="text-2xl font-bold">Page d'accueil</h1>;
 }
---
+++ src/firebase.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMnlpV8RSy6vZtavBGqKLnc1mBLInRZLg",
+  authDomain: "junior-process.firebaseapp.com",
+  projectId: "junior-process",
+  storageBucket: "junior-process.appspot.com",
+  messagingSenderId: "361799028758",
+  appId: "1:361799028758:web:6e7b01abb38760c1e86537"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
